511f8ee49cec18bbf828c321607e62a3227d33b7
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2023-09-14 18:41:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function calculateMinCost() {
   //your code here
-  
-  
+
+	let str = document.getElementById("rope-lengths").value;
+	const arr = str.split(",").map((x) => Number(x))
+	let res = 0;
+	while(arr.length > 1){
+	   arr.sort((a,b) => b-a);
+		let x = arr.pop();
+		let y = arr.pop();
+		let sum = x + y;
+		if(arr.length != 0){
+			res += sum;
+		}
+		arr.push(sum)
+	}
+
+	res += arr.pop()
+	
+  document.getElementById("result").innerHTML = res; 
   
 }  
